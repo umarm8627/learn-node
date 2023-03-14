e10ba2bce0cbbdf287309e1a3d3efa5a43e9c4e6
Umer <umarmnextbyte@gmail.com>
2023-03-14 13:32:22 +0500

    event-loops-example

---
+++ event-loop-examples/3-setInterval.js
@@ -0,0 +1,9 @@
+setInterval(()=>{
+    console.log('hello world');
+},2000);
+console.log('I will run first');
+
+// run every 2 seconds 
+// process stays alive unless
+// Kill Process CONTROL + C
+// unexpected error
